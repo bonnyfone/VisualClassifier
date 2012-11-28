0e77753814b9be8433b718971ec24b1c15c8139d
Stefano Bonetta <bonnyfone@gmail.com>
2012-11-28 18:12:17 +0100

    Classifier and code generator completed

--- src/org/visualclassifier/generator/ClassifierGenerator.java
+++ src/org/visualclassifier/generator/ClassifierGenerator.java
@@ -1,6 +1,6 @@
 package org.visualclassifier.generator;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -17,13 +17,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.text.View;
 
 import org.visualclassifier.DataHandler;
 
@@ -32,15 +32,20 @@ public class ClassifierGenerator extends JFrame {
 
 	private static final long serialVersionUID = -4087546224477720561L;
 
+
+
 	private JButton btnMerge;
 	private JButton btnGenerate;
 	private JButton btnSfoglia;
 	private JTextArea txtLog;
 	private JTextArea txtGenerateFile;
-	
-	public static String TAG_TREE_INIT = "------------------";
-	public static String TAG_TREE_END  = "Number of Leaves";
-	
+	private String fileName;
+
+	private static final String TAG_ATTR_INIT = "Attributes";
+	private static final String TAG_ATTR_END = "Test mode";
+	public static final String TAG_TREE_INIT = "------------------";
+	public static final String TAG_TREE_END  = "Number of Leaves";
+
 
 	public ClassifierGenerator(){
 		init();
@@ -83,10 +88,11 @@ public class ClassifierGenerator extends JFrame {
 			btnGenerate.setEnabled(false);
 			addLog("Elaborating "+fs.getSelectedFile().getAbsolutePath());
 			addLog("Weka is Working...");
-			
+
 			Thread t = new Thread(){
 				public void run(){
 					try {
+						fileName = fs.getSelectedFile().getName();
 						elaborate(fs.getSelectedFile());
 					} catch (IOException e) {
 						e.printStackTrace();
@@ -116,32 +122,164 @@ public class ClassifierGenerator extends JFrame {
 		StringBuilder sb = new StringBuilder();
 		boolean tree = false;
 		while ((line=buf.readLine())!=null) {
-			if(line.contains(TAG_TREE_END)) tree=false;			
+			if(line.contains(TAG_TREE_END)) tree=false;
 			if(tree){
 				//addLog(line);
 				sb.append(line+"\n");
 			}
-			if(line.startsWith(TAG_TREE_INIT))	tree=true;
 			
+			if(line.startsWith(TAG_TREE_INIT))	tree=true;
+
 		}
-		Viewer v = new Viewer(sb.toString().trim());
+		Viewer v = new Viewer(sb.toString().trim(), Color.black);
 		v.setTitle("J48 Tree (" + selectedFile.getAbsolutePath()+")");
 		v.setVisible(true);
-		
+
 		addLog("Generating source code...");
 		generateSourceCode(sb.toString().trim());
 	}
-	
-	private void generateSourceCode(String tree){
-		//TODO here! 
-		addLog("Generation complete :)");
+
+	private String header(){
+		
+		return "inline bool classify_" + fileName.replace(".arff", "") +" (Cluster* cluster){\n" ;
 	}
 
+	private void generateSourceCode(String tree){
+		ArrayList<String> vars = new ArrayList<String>();
+		
+		tree=tree.replace(":", " :");
+		StringBuilder sb = new StringBuilder();
+		sb.append(header());
+
+		String object = "cluster->";
+		Scanner scanner = new Scanner(tree);
+
+		int level = 0;
+		int prevLevel = 0;
+		boolean previusReturned = false;
+		boolean firstLine = true;
+		while (scanner.hasNextLine()) {
+			String rawLine = scanner.nextLine();
+			String statement = "";
+			level = countOccurrences(rawLine, '|');
+			boolean returnLine = rawLine.contains(":");
+			Scanner line = new Scanner(rawLine);
+
+			if(returnLine || prevLevel<level || firstLine){
+				
+				if(( previusReturned && (prevLevel==level) ) ||  (returnLine && (prevLevel>level)) ){
+					statement = "else return ";
+					if(rawLine.contains("negative"))statement += "false;\n";
+					else if(rawLine.contains("positive")){
+						statement += "true;\n";
+					}
+					sb.append(tabbing(level)+statement);
+					prevLevel = level;
+					previusReturned = returnLine;
+					continue;
+				}
+
+				statement = "if(";
+
+				int counter = 0;
+				while(line.hasNext()){
+					String token = line.next();
+					
+					if(isVar(token) && !vars.contains(token))vars.add(token);
+					
+					if(counter < level+3){ 
+						if(!token.equals("|")){
+							statement += (" " +token);
+							if(!line.hasNext()){
+								statement +=") \n";	
+							}
+						}
+					}else{
+						if(returnLine){
+							if(token.contains("negative"))
+								statement += ") return false;\n";
+							else if(token.contains("positive"))
+								statement += ") return true;\n";
+						}else{
+							statement +=") {\n"; //??
+						}
+					}
+					counter++;
+
+				}
+				sb.append(tabbing(level)+statement);
+			}
+			else{
+				
+				/*
+				int diff = prevLevel - level;
+				if(diff>0){
+					int tmp = prevLevel;
+					for(int i=0;i<diff;i++){
+						//sb.append("\n"+tabbing(tmp-1)+"}\n");
+						sb.append("\n");
+						tmp--;
+					}
+				}
+				*/
+				//sb.append(tabbing(level)+"else{\n");
+				sb.append(tabbing(level)+"else\n");
+			}
+			firstLine = false;
+			prevLevel = level;
+			previusReturned =returnLine;
+
+		}
+		/*
+		int tmp = prevLevel;
+		for(int i=0;i<prevLevel;i++){
+			//sb.append("\n"+tabbing(tmp-1)+"}\n");
+			sb.append("\n");
+			tmp--;
+		}
+		*/
+
+		sb.append("\n}");
+		String out = sb.toString();
+		for(String v : vars){
+			out = out.replace(v, object+v);
+		}
+		
+		Viewer v = new Viewer(out, Color.blue);
+		v.setVisible(true);
+		v.setTitle("Classifier C++ snippet");
+		addLog("Generation complete :)");
+	}
+	
+
+	private String tabbing(int howMany){
+		String ris="";
+		for(int i=0;i<howMany+1;i++)ris += "\t";
+		return ris;
+	}
 
 	private void addLog(String s){
 		txtLog.insert(s+"\n", 0);
 	}
 
+	public static boolean isVar(String str){
+		if(!isNumeric(str) && !str.contains("=") && !str.contains(">") && !str.contains("<"))return true;
+		return false;
+	}
+	
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
+	}
+	
 	private void init(){
 		setSize(600, 250);
 		setTitle("Classifier Generator");
@@ -165,6 +303,18 @@ public class ClassifierGenerator extends JFrame {
 		add(a);
 		add(txtLog);
 
+	}
+
+	public static int countOccurrences(String haystack, char needle){
+		int count = 0;
+		for (int i=0; i < haystack.length(); i++)
+		{
+			if (haystack.charAt(i) == needle)
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 
--- src/org/visualclassifier/generator/Viewer.java
+++ src/org/visualclassifier/generator/Viewer.java
@@ -1,16 +1,57 @@
 package org.visualclassifier.generator;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class Viewer extends JFrame{
+public class Viewer extends JFrame implements ClipboardOwner{
 	
-	public Viewer(String content){
+	private static final long serialVersionUID = -8375144330131508984L;
+
+	public Viewer(String content, Color txtColor){
 		setSize(700, 600);
 		setLayout(new GridLayout(1,1));
-		add(new JTextArea(content));
+		final JTextArea t = new JTextArea(content);
+		JScrollPane scroll = new JScrollPane(t);
+		t.setTabSize(2);
+		t.setForeground(txtColor);
+		setLayout(new BorderLayout());
+		add(scroll,BorderLayout.CENTER);
+		
+		JButton btn = new JButton("Copy");
+		add(btn,BorderLayout.SOUTH);
+		
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				setClipboardContents(t.getText());
+			}
+		});
+		
+		
 	}
 
+	public void setClipboardContents( String aString ){
+	    StringSelection stringSelection = new StringSelection( aString );
+	    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+	    clipboard.setContents( stringSelection, this );
+	  }
+
+	@Override
+	public void lostOwnership(Clipboard arg0, Transferable arg1) {
+		//?
+	}
+	
 }
